ebcc925a8f275809116595df0294dd3831bf50f2
Alfonso <abrown.glez@gmail.com>
2019-03-29 22:28:24 -0600

    Cleanup

--- Gym-tests/Spaces2.py
+++
@@ -1,7 +0,0 @@
-#This introspection can be helpful to write generic code that works for many different environments. Box and Discrete are the most common Spaces. You can sample from a Space or check that something belongs to it:
-
-from gym import spaces
-space = spaces.Discrete(8) # Set with 8 elements {0, 1, 2, ..., 7}
-x = space.sample()
-assert space.contains(x)
-assert space.n == 8
--- roboschool/naoenv_run.py
+++
@@ -1,9 +0,0 @@
-import gym
-import roboschool
-import time
-
-env = gym.make('RoboschoolNaoForwardWalk-v1')
-env.reset()
-env.render()
-while True:
-    env.step(env.action_space.sample())

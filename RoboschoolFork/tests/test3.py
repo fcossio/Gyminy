972729853271cf3679456c7c4b205c73c81ab577
fcossio <oissoc.ref@gmail.com>
2019-08-19 20:28:54 -0500

    timetest

---
+++ RoboschoolFork/tests/test3.py
@@ -0,0 +1,42 @@
+import gym, roboschool, roboschoolfork_nao
+import tensorflow as tf
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines import PPO2
+from time import time
+# multiprocess environment
+n_cpu = 2
+env = SubprocVecEnv([lambda: gym.make('RoboschoolNaoForwardWalk-v1') for i in range(n_cpu)])
+#
+initial_timestep = 0
+total_train_timesteps = 10000
+activation_function = tf.nn.tanh
+net_arch = [256,128,64]
+# checkpoint_timesteps = 10000
+
+start_time = time()
+
+
+policy_kwargs = dict(act_fun=activation_function, net_arch = net_arch)
+model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log="./ppo2_NaoForwardWalk12jul",
+   policy_kwargs=policy_kwargs)
+# model = PPO2.load("ppo2_NaoForwardWalk11jul.pkl", env = env, tensorboard_log="./ppo2_NaoForwardWalk")
+model.learn(total_timesteps=total_train_timesteps)
+model.save("ppo2_NaoForwardWalk12jul.pkl")
+print("Saved")
+
+end_time = time()
+
+print("Elapsed time training", end_time - start_time)
+# del model # remove to demonstrate saving and loading
+#
+# model = PPO2.load("ppo2_NaoForwardWalk12jul.pkl")
+# env = gym.make('RoboschoolNaoForwardWalk-v1')
+# # Enjoy trained agent
+# obs = env.reset()
+# while True:
+#     action, _states = model.predict(obs)
+#     obs, rewards, dones, info = env.step(action)
+#     if dones:
+#         obs = env.reset()
+#     env.render()
